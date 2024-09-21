4168a5c6a773ba1e6b4bf740da456883be78c651
tw1zzR <regenuss@gmail.com>
2024-09-21 17:12:30 +0300

    just repository

--- lab3.1.py
+++ lab3.1.py
@@ -1,4 +1,6 @@
-a = float(input("Введіть перше двозначне число a: "))
-b = float(input("Введіть друге двозначне число b: "))
 
-result =
+a = int(input("Введіть перше двозначне число a: "))
+b = int(input("Введіть друге двозначне число b: "))
+
+result = int(str(a)[0]) * int(str(b)[0])
+print(f"Відповідь: {result}.")
